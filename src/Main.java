a8206b5a6419dfe307d2135ba1d37f322c77df05
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-02-02 22:54:50 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter File Name");
+        String fileName= input.next();
+        try {
+            Scanner file=new Scanner(new File(fileName));
+            HashMap<Character,Integer> hm=new HashMap<>();
+            while(file.hasNext()){
+                String word=file.next();
+                for(char ch:word.toCharArray()){
+                    if(!hm.containsKey(ch))
+                        hm.put(ch,0);
+                    hm.put(ch,hm.get(ch)+1);
+                }
+            }
+            PrintWriter printWriter=new PrintWriter("output.txt");
+            printWriter.println("The file "+fileName+" contains: ");
+            for(Map.Entry<Character,Integer> obj:hm.entrySet()){
+                printWriter.println(obj.getKey()+" has repeated "+obj.getValue()+" times");
+            }
+            file.close();
+            printWriter.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
